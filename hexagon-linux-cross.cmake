e1cddf0fd4867cde21d7e7f7995b9c21c5fed107
Brian Cain <bcain@quicinc.com>
2024-07-12 08:25:37 -0700

    Disable XRay, Memprof, sanitizer builds
    Since llvm-project 71c5453fa9a1 ([compiler-rt] Add hexagon architecture
    to cmake (#98650), 2024-07-12) we started building additional content
    in compiler-rt and see failures in sanitizer_common.  Disable XRay, Memprof,
    sanitizers in order to avoid building sanitizer_common.
    
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- hexagon-linux-cross.cmake
+++ hexagon-linux-cross.cmake
@@ -1,5 +1,8 @@
 
 set(CMAKE_SYSTEM_NAME Linux CACHE STRING "")
+set(CMAKE_C_COMPILER_TARGET hexagon-unknown-linux-musl CACHE STRING "")
+set(CMAKE_CXX_COMPILER_TARGET hexagon-unknown-linux-musl CACHE STRING "")
+
 set(CMAKE_C_COMPILER hexagon-unknown-linux-musl-clang CACHE STRING "")
 set(CMAKE_SIZEOF_VOID_P 4 CACHE STRING "")
 set(CMAKE_CXX_COMPILER hexagon-unknown-linux-musl-clang++ CACHE STRING "")
