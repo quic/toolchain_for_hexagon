a9200a58c031c7bcf5b49dd1f1564662afd9a73f
Brian Cain <bcain@quicinc.com>
2024-09-13 13:28:08 -0700

    Add slirp support for QEMU
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -243,6 +243,7 @@ build_qemu() {
 	CC=$(which gcc) \
 	PATH=${TOOLCHAIN_BIN}:${PATH} \
 	../qemu/configure --enable-fdt --disable-capstone --disable-guest-agent \
+	                  --enable-slirp \
 	                  --disable-containers \
 	                  --python=$(which python3.8) \
 		--target-list=hexagon-softmmu,hexagon-linux-user --prefix=${TOOLCHAIN_INSTALL}/x86_64-linux-gnu \
