d76d9463d9588d12b40ecbe2211a794f8a3ca2d5
Brian Cain <brian.cain@oss.qualcomm.com>
2024-12-13 15:01:49 -0800

    Revert "build-rootfs.sh: Switch to produce zstd instead of xz"
    QEMU functional tests uses python tarfile which doesn't recognize
    zstd automatically (yet?).
    
    This reverts commit ffa727b3efec0d053b81a58b8b483c8d94702704.

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -230,10 +230,11 @@ build_kernel
 
 
 if [[ ${MAKE_TARBALLS-0} -eq 1 ]]; then
-    tar c -C $(dirname ${ROOT_INSTALL_REL}) $(basename ${ROOT_INSTALL_REL}) | zstd --fast -T0 > ${RESULTS_DIR}/hexagon_rootfs_${STAMP}.tar.zst
+    tar c -C $(dirname ${ROOT_INSTALL_REL}) $(basename ${ROOT_INSTALL_REL}) | xz -e9T0 > ${RESULTS_DIR}/hexagon_rootfs_${STAMP}.tar.xz
+#   XZ_OPT="-8 --threads=0" tar c ${RESULTS_DIR}/hexagon_rootfs_${STAMP}.tar.xz  -C $(dirname ${ROOT_INSTALL_REL}) $(basename ${ROOT_INSTALL_REL})
 
     cd ${RESULTS_DIR}
-    sha256sum hexagon_rootfs_${STAMP}.tar.zst > hexagon_rootfs_${STAMP}.tar.zst.sha256
+    sha256sum hexagon_rootfs_${STAMP}.tar.xz > hexagon_rootfs_${STAMP}.tar.xz.sha256
 fi
 
 if [[ ${TEST_TOOLCHAIN-0} -eq 0 ]]; then
