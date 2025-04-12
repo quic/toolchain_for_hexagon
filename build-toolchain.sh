776702487efb3b902822601fba24f86c590e40de
Brian Cain <brian.cain@oss.qualcomm.com>
2025-04-12 10:58:13 -0500

    Add "virtual" multilib configuration
    clang adds search paths like `-L/home/user/src/toolchain_for_hexagon/clang+llvm-21.0.0-cross-hexagon-unknown-linux-musl/x86_64-linux-gnu/bin/../target/hexagon/lib/v68` and this will result in warnings like below from eld:
    
    	Note: can not open search directory `/home/user/src/toolchain_for_hexagon/clang+llvm-21.0.0-cross-hexagon-unknown-linux-musl/x86_64-linux-gnu/bin/../target/hexagon/lib/v68'
    
    Let's create symlinks to resemble the multilib paths expected by clang,
    to avoid this warning from eld.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -113,6 +113,17 @@ add_symlinks() {
 #	ln -sf --relative ${linkdir}/clang ${linkdir}/hexagon-unknown-none-elf-clang++
 }
 
+add_multilib_symlinks() {
+	linkdir=${1}
+
+	cd ${linkdir}
+	for arch in v68 v69 v71 v73 v75 v79
+	do
+		ln -sf --relative  ../usr/lib ./${arch}
+	done
+	cd -
+}
+
 build_clang_rt_builtins() {
 	cd ${BASE}
 
@@ -182,6 +193,9 @@ build_musl_headers() {
 	cd ${HEX_SYSROOT}/..
 	ln -sf hexagon-unknown-linux-musl hexagon
 	ln -sf hexagon-unknown-linux-musl hexagon-linux-musl
+	DEST_TGT_LIB=${HEX_SYSROOT}/lib
+	mkdir -p ${DEST_TGT_LIB}
+	add_multilib_symlinks ${DEST_TGT_LIB}
 }
 
 build_musl() {
