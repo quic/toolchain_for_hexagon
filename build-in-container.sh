690674c053a1cded5d4fc8db9864fdb073299a1b
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-14 00:49:16 -0600

    Skip windows QEMU build for now
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- build-in-container.sh
+++ build-in-container.sh
@@ -19,13 +19,15 @@ DOCKER_BUILD_ARGS="
 
 #build
 docker build ${DOCKER_BUILD_ARGS} -t hexagon:latest -f ./Dockerfile .
-docker build ${DOCKER_BUILD_ARGS} -t hexagon-win:latest -f ./cross-win/Dockerfile .
+echo skip windows qemu for now
+echo docker build ${DOCKER_BUILD_ARGS} -t hexagon-win:latest -f ./cross-win/Dockerfile .
 
 #debug
 docker images
 
 #extract artifacts
-for name in hexagon hexagon-win; do
+#for name in hexagon hexagon-win; do
+for name in hexagon; do
     docker rm -f tmp_container || /bin/true
     docker create --name tmp_container $name:latest
     docker cp tmp_container:/usr/local/hexagon-artifacts ./hexagon-artifacts
